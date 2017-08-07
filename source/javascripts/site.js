d64e2f9fd3a20ae33cadd11bd62f785ea0b1d334
rum-n <manev.ru@gmail.com>
2017-08-07 23:02:33 +0300

    arrow and back button

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -8,12 +8,7 @@
 
 //////////////// ADDED BY ME
 
-$(function() {
-  $('a[href*=#]').on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-  });
-});
+
 
 //////////////// END OF ADDED BY ME
 
