7f3606c1dd307631e74dc1a0f46ecd1aabc9015e
rum-n <manev.ru@gmail.com>
2017-09-09 19:41:51 +0300

    contact form

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -7,6 +7,32 @@
 
 //////////////// ADDED BY ME
 
+// FADE IN ON LOAD PAGE
+
 $(document).ready(function(){$("body").hide().fadeIn(2000);});
+
+// END FADE IN ON LOAD PAGE
+
+// CONTACT FORM
+
+$(function() {
+  
+  // contact form animations
+  $('#contact').click(function() {
+    $('#contactForm').fadeToggle();
+  })
+  $(document).mouseup(function (e) {
+    var container = $("#contactForm");
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.fadeOut();
+    }
+  });
+  
+});
+
+// END CONTACT FORM
 
 //////////////// END OF ADDED BY ME
